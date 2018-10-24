062107b87536ed79e9e13b08af8c15b4adc8881e
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2018-10-24 09:21:52 +0200

    chore(ci): build charts with travis and publish to gh-pages branch

---
+++ .travis/deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+PACKAGE_DIR=/tmp/out
+mkdir -p ${PACKAGE_DIR}
+
+# Package charts (except veidemann)
+find repo/ -name Chart.yaml -maxdepth 1 -printf "%h\n" | xargs helm package -u -d ${PACKAGE_DIR}
+
+# Checkout gh-pages
+git remote set-branches origin gh-pages
+git fetch
+git checkout gh-pages
+
+# Copy new packages to gh-pages
+cp ${PACKAGE_DIR}/* .
+
+# Create/merge helm repository index
+helm repo index --url ${REPO_URL} --merge index.yaml .
+
+# Stage packages and index file
+git add *.tgz index.yaml
+
+git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
+
+# Push to gh-pages with ssh credentials
+git push git@github.com:${TRAVIS_REPO_SLUG}.git
